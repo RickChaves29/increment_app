ced710499e1301b2100b008bd15ad4f6b07b985e
Henrique chaves <canaldomonza@gmail.com>
2022-05-16 15:10:01 -0300

    added test for counter entity

--- test/domain/entities/counter_entity_test.dart
+++ test/domain/entities/counter_entity_test.dart
@@ -2,9 +2,23 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:increment_app/domain/entities/counter_entity.dart';
 
 void main() {
-  test('se a variavel counter começa com o valor de 0', () {
-    CounterEntity counterEntity = CounterEntity();
-    int result = counterEntity.count;
-    expect(result, 0);
+  group('Counter Entity', () {
+    test('se a variavel counter começa com o valor de 0', () {
+      CounterEntity counterEntity = CounterEntity();
+      int result = counterEntity.count;
+      expect(result, 0);
+    });
+    test('se o metodo increment está adicionando mais 1 a variavel count', () {
+      CounterEntity counterEntity = CounterEntity();
+      counterEntity.increment();
+      int result = counterEntity.count;
+      expect(result, 1);
+    });
+    test('se o metodo increment está adicionando mais 1 a variavel count', () {
+      CounterEntity counterEntity = CounterEntity();
+      counterEntity.decrement();
+      int result = counterEntity.count;
+      expect(result, -1);
+    });
   });
 }
